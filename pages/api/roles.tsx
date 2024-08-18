d40a9e8af7632d0b409f77ae9ceb58800d648902
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2024-08-18 17:26:18 +0800

    update

--- pages/api/roles.tsx
+++ pages/api/roles.tsx
@@ -1,39 +1,41 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { createClient } from '@supabase/supabase-js'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = ``
-const supabaseKey = ``
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = `https://vajvudbmcgzbyivvtlvy.supabase.co`;
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhanZ1ZGJtY2d6YnlpdnZ0bHZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3NTMxOTAsImV4cCI6MjAzNjMyOTE5MH0.NSt8sCe1d6a2wKsHk_nROH9jtkuSzChc-2V8Nj_V150`;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-type ResponseData = {
-  message: string
-} | any
+type ResponseData =
+  | {
+      message: string;
+    }
+  | any;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     if (!req.body.title) {
-      return res.status(400).json({ message: 'Title must not be null' })
+      return res.status(400).json({ message: "Title must not be null" });
     }
 
     try {
       const { data, error } = await supabase
-        .from('jobs')
-        .select('*')
-        .ilike('title', `%${req.body.title}%`)
-        .limit(5)
+        .from("jobs")
+        .select("*")
+        .ilike("title", `%${req.body.title}%`)
+        .limit(5);
 
       if (error) {
-        throw error
+        throw error;
       }
 
-      return res.status(200).json(data)
+      return res.status(200).json(data);
     } catch (error: any) {
-      return res.status(500).json({ message: error.message })
+      return res.status(500).json({ message: error.message });
     }
   } else {
-    return res.status(400).json({ message: 'Method not supported' })
+    return res.status(400).json({ message: "Method not supported" });
   }
 }
