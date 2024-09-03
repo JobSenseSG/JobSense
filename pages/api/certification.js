c3287fcab14c6f3e1ff0fa4d7ca7a056e6a05157
Adeeb <adityaadeeb04@gmail.com>
2024-09-03 17:34:47 +0800

    making it query from supabase

--- pages/api/certification.js
+++ pages/api/certification.js
@@ -1,18 +1,25 @@
-// pages/api/events.js
-const pool = require('../../lib/db.js'); // Update the path to where your actual db config file is located
+// pages/api/certification.js
+
+import { createClient } from '@supabase/supabase-js';
+
+// Initialize Supabase client
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
-    const client = await pool.connect();
     try {
-        // Replace 'your_table' with the actual table name you want to query
-        const result = await client.query('SELECT * FROM certificate_evaluations.certificate_info ');
-        res.status(200).json(result.rows);
+        const { data, error } = await supabase
+            .from('certifications')
+            .select('*');
+
+        if (error) {
+            throw error;
+        }
+
+        res.status(200).json({ certifications: data }); // Send the certifications array wrapped in an object
     } catch (error) {
-        // It's good to handle errors and send a corresponding response
+        console.error('Error fetching certifications:', error.message);
         res.status(500).json({ error: error.message });
-    } finally {
-        client.release();
     }
 }
-
-
